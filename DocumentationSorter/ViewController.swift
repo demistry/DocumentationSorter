c809bcb241153cd038a7542b5aa4e645170c5e63
Demistry <dave11ilen@gmail.com>
2019-04-17 15:33:33 +0100

    Initial Commit

---
+++ DocumentationSorter/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DocumentationSorter
+//
+//  Created by David Ilenwabor on 17/04/2019.
+//  Copyright © 2019 Davidemi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
