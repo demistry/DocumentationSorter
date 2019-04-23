97d9defb5c068269e52acee35cece78206e57702
Demistry <dave11ilen@gmail.com>
2019-04-23 15:18:17 +0100

    Added core data entity for the selected framework

---
+++ DocumentationSorter/SelectedFrameworkObjects.swift
@@ -0,0 +1,16 @@
+//
+//  SelectedFrameworkObjects.swift
+//  DocumentationSorter
+//
+//  Created by David Ilenwabor on 23/04/2019.
+//  Copyright © 2019 Davidemi. All rights reserved.
+//
+
+import Foundation
+
+
+struct SelectedFrameworkObject{
+    var categoryTitle : String
+    var frameworkTitle : String
+    var dateOfSelection : Date
+}
